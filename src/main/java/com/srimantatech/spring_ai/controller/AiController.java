248e00a71a31bc49c95c7f28417b90ed7cd9162a
Srimanta Gangopadhyay <srimantaganguly02@gmail.com>
2026-04-08 02:05:23 +0530

    Implement a InMemoryChatAdvisor

--- src/main/java/com/srimantatech/spring_ai/controller/AiController.java
+++ src/main/java/com/srimantatech/spring_ai/controller/AiController.java
@@ -3,6 +3,9 @@ package com.srimantatech.spring_ai.controller;
 import com.srimantatech.spring_ai.advisor.SimpleLoggerAdvisor;
 import com.srimantatech.spring_ai.entity.Genre;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,12 @@ public class AiController {
     private final ChatClient client;
 
     public AiController (ChatClient.Builder chatClientBuilder) {
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder().build();
+
         this.client = chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
                 .build();
     }
 
@@ -43,5 +50,14 @@ public class AiController {
                 .call()
                 .entity(new ParameterizedTypeReference<List<Genre>>() {});
 
+    }
+
+    @GetMapping("/memory-prompt")
+    public String memoryPrompt (String userId, String userInput) {
+        return this.client.prompt()
+                .user(userInput)
+                .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, userId))
+                .call()
+                .content();
     }
 }
